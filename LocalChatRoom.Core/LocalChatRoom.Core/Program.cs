28b49263de6cfc61a3b9e37988f97c04d8419732
Yuki Sakumoto <cooking.of.the.magic.11@gmail.com>
2018-09-18 21:09:59 +0900

    Add first projects.

---
+++ LocalChatRoom.Core/LocalChatRoom.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocalChatRoom.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

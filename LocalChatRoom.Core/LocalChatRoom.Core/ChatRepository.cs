8ea12c264881e7f040098fd8562af9073a907321
Yuki Sakumoto <cooking.of.the.magic.11@gmail.com>
2018-09-18 22:59:02 +0900

    Add core project.

---
+++ LocalChatRoom.Core/LocalChatRoom.Core/ChatRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalChatRoom.Core
+{
+    class ChatRepository
+    {
+        bool isObserve = false;
+        bool failedToRead = false;
+        string changedFilePath = "";
+        List<Action<string>> actions = new List<Action<string>>();
+
+        public void AddActionWhenRepositoryChanged(Action<string> action)
+        {
+            actions.Add(action);
+        }
+
+        public void StartObserve(string directoryPath)
+        {
+            if (isObserve) return;
+            isObserve = true;
+            failedToRead = false;
+            FileSystemWatcher watcher = new FileSystemWatcher(directoryPath);
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "*.chat";
+            watcher.Changed += new FileSystemEventHandler(async (object sender, FileSystemEventArgs e) =>
+            {
+                changedFilePath = e.FullPath;
+                try
+                {
+                    await Task.Delay(500);
+                    ActionAll(changedFilePath);
+                }
+                catch (Exception exc)
+                {
+                    failedToRead = true;
+                }
+            });
+
+            watcher.EnableRaisingEvents = true;
+            Task.Run(CheckIsUpdatedCorrectory);
+        }
+
+        async Task CheckIsUpdatedCorrectory()
+        {
+            while (isObserve)
+            {
+                await Task.Delay(5000);
+                if (failedToRead == false) continue;
+                try
+                {
+                    ActionAll(changedFilePath);
+                }
+                catch (Exception e)
+                {
+                    return;
+                }
+                failedToRead = false;
+            }
+        }
+
+        string FileFileter
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var result ="" + today.Year + today.Month + today.Day + ".chat";
+                return result;
+            }
+        }
+
+        void ActionAll(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                var value = reader.ReadToEnd();
+                reader.Close();
+                actions.ForEach(a => a(value));
+                reader.Close();
+            }
+        }
+
+        public void StopObserve() => isObserve = false;
+    }
+}

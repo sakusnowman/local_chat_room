0e4a524b318ac73dee67af23899b095678bf16cc
Yuki Sakumoto <cooking.of.the.magic.11@gmail.com>
2018-09-19 21:58:44 +0900

    delete .core project.

---
+++ LocalChatRoom/LocalChatRoom/ChatRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalChatRoom
+{
+    class ChatRepository
+    {
+        Process process;
+        private readonly Action<string> actionWhenRecivedNewMessage;
+        private readonly string chatDirectoryPath;
+        private string notSendMessage = "";
+        public ChatRepository(Action<string> actionWhenRecivedNewMessage, string chatDirectoryPath)
+        {
+            this.actionWhenRecivedNewMessage = actionWhenRecivedNewMessage;
+            this.chatDirectoryPath = chatDirectoryPath;
+        }
+
+        public void StartChat(string exePath)
+        {
+            process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.FileName = exePath;
+            process.StartInfo.Arguments = chatDirectoryPath;
+            process.OutputDataReceived += new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) =>
+            {
+                actionWhenRecivedNewMessage(e.Data);
+            });
+
+            process.Start();
+        }
+
+        public void EndChat()
+        {
+            process.StandardInput.WriteLine("q");
+        }
+
+        public void SendMessage(string message)
+        {
+            var today = DateTime.Today;
+            notSendMessage = message;
+            try
+            {
+                WriteMessage();
+            }
+            catch { }
+        }
+
+        async Task SendMessageAgainIfNeed()
+        {
+            while (process.HasExited == false)
+            {
+                await Task.Delay(1000);
+                try
+                {
+                    WriteMessage();
+                }
+                catch { }
+            }
+        }
+
+        void WriteMessage()
+        {
+            if (string.IsNullOrEmpty(notSendMessage)) return;
+            process.StandardInput.WriteLine(notSendMessage);
+            var today = DateTime.Today;
+            using (var writer = new StreamWriter(Path.Combine(chatDirectoryPath, "" + today.Year + today.Month + today.Day + ".chat"), true, Encoding.Default))
+            {
+                writer.WriteLine(notSendMessage);
+                notSendMessage = "";
+            }
+        }
+    }
+}
--- LocalChatRoom/LocalChatRoom/MainWindow.xaml.cs
+++ LocalChatRoom/LocalChatRoom/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace LocalChatRoom
     /// </summary>
     public partial class MainWindow : Window
     {
+        ChatRepository chatRepository;
         public MainWindow()
         {
             InitializeComponent();
+            chatRepository = new ChatRepository(s =>
+            {
+                this._chatMessageText.Text += s;
+            }, @"C:\ChatDemo");
+            chatRepository.StartChat(@"C:\Users\Owner\git\local_chat_room\LocalChatRoom.Core\LocalChatRoom.Core\bin\Release\netcoreapp2.0\win-x64\publish\LocalChatRoom.Core.exe");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void _myMessageText_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyboardDevice.IsKeyDown(Key.LeftCtrl) || e.KeyboardDevice.IsKeyDown(Key.RightCtrl))
+            {
+                if (e.Key.Equals(Key.M))
+                {
+                    SendMessage();
+                }
+            }
+        }
+
+        private void SendMessage()
+        {
+            chatRepository.SendMessage(_myMessageText.Text);
+            this._myMessageText.Text = "";
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            chatRepository.EndChat();
         }
     }
 }
